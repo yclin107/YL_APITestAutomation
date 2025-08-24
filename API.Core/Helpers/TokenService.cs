6cd41dd0ee2bae13bc0462854bdebc696117de0e
yclin107 <yang92cr@gmail.com>
2025-08-23 21:49:23 -0600

    Fix ProfileManager Variable Conflict

--- API.Core/Helpers/TokenService.cs
+++ API.Core/Helpers/TokenService.cs
@@ -80,7 +80,6 @@ namespace API.Core.Helpers
         
         private static async Task<TenantProfile?> GetProfileFromEnvironment(ProfileManager profileManager)
         {
-            var profileManager = new ProfileManager();
             var profilePath = Environment.GetEnvironmentVariable("TEST_PROFILE");
             var masterPassword = Environment.GetEnvironmentVariable("MASTER_PASSWORD");
             
